7f8babad181bbfb4bfa5ee18602903b09e55294d
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-02-04 01:26:52 +0530

    UI Created

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,260 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(children: [
+            Align(
+              alignment: const AlignmentDirectional(15, -0.3),
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                    color: Colors.deepPurple, shape: BoxShape.circle),
+              ),
+            ),
+            Align(
+              alignment: const AlignmentDirectional(-15, -0.3),
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                    color: Colors.deepPurple, shape: BoxShape.circle),
+              ),
+            ),
+            Align(
+              alignment: const AlignmentDirectional(0, -1.2),
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                    color: Colors.orange, shape: BoxShape.rectangle),
+              ),
+            ),
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaY: 100, sigmaX: 100),
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.transparent),
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Place",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w300),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text(
+                    "Good Morning",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Image.asset('assets/1.png'),
+                  Center(
+                    child: Text(
+                      "21 Degree C",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      "ThunderStorm",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Center(
+                    child: Text(
+                      "Friday, 2 February 2020 - 7:43PM",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset(
+                            "assets/11.png",
+                            scale: 8,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Sunrise",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                "5:43 AM",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            "assets/12.png",
+                            scale: 8,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Sunset",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                "5:43 PM",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Divider(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset(
+                            "assets/13.png",
+                            scale: 8,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Temp Max",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                "12 Degree C",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            "assets/14.png",
+                            scale: 8,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Temp Min",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                "8 Degree C ",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
   }
 }
