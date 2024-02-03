865b917d3574bdfd10c3811ca4acdcf8679df163
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-02-04 02:02:29 +0530

    GeoLocator Setup

--- lib/bloc/weather_event.dart
+++ lib/bloc/weather_event.dart
@@ -8,4 +8,10 @@ sealed class WeatherEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class FetchWeather extends WeatherEvent {}
+class FetchWeather extends WeatherEvent {
+  final Position position;
+
+  FetchWeather(this.position);
+  @override
+  List<Object> get props => [position];
+}
